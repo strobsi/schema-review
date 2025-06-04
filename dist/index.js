e4e1553f926df86d091946a0c9656966c82656cf
David Boyne <boyneyy123@gmail.com>
2025-06-04 11:41:46 +0100

    fix(sdk): fixed issue getting producers and consumers for messages

--- dist/index.js
+++ dist/index.js
@@ -1,7 +1,7 @@
 require('./sourcemap-register.js');/******/ (() => { // webpackBootstrap
 /******/ 	var __webpack_modules__ = ({
 
-/***/ 1488:
+/***/ 1757:
 /***/ ((module) => {
 
 function webpackEmptyAsyncContext(req) {
@@ -15,7 +15,7 @@ function webpackEmptyAsyncContext(req) {
 }
 webpackEmptyAsyncContext.keys = () => ([]);
 webpackEmptyAsyncContext.resolve = webpackEmptyAsyncContext;
-webpackEmptyAsyncContext.id = 1488;
+webpackEmptyAsyncContext.id = 1757;
 module.exports = webpackEmptyAsyncContext;
 
 /***/ }),
@@ -11592,7 +11592,7 @@ function updateToolCallResult({
 
 /***/ }),
 
-/***/ 5672:
+/***/ 8945:
 /***/ ((module, __unused_webpack_exports, __nccwpck_require__) => {
 
 "use strict";
@@ -12273,6 +12273,7 @@ var addMessageToChannel = (directory, collection) => async (id, _message, versio
 // src/messages.ts
 var import_node_path8 = __nccwpck_require__(6928);
 var import_gray_matter4 = __toESM(__nccwpck_require__(1847));
+var import_semver3 = __nccwpck_require__(3360);
 var getMessageBySchemaPath = (directory) => async (path4, options) => {
   const pathToMessage = (0, import_node_path8.dirname)(path4);
   try {
@@ -12312,7 +12313,7 @@ var getProducersAndConsumersForMessage = (directory) => async (id, version) => {
   for (const service of services) {
     const servicePublishesMessage = service.sends?.some((_message) => {
       if (_message.version) {
-        return _message.id === message.id && _message.version === message.version;
+        return _message.id === message.id && (0, import_semver3.satisfies)(message.version, _message.version);
       }
       if (isMessageLatestVersion && _message.id === message.id) {
         return true;
@@ -12321,7 +12322,7 @@ var getProducersAndConsumersForMessage = (directory) => async (id, version) => {
     });
     const serviceSubscribesToMessage = service.receives?.some((_message) => {
       if (_message.version) {
-        return _message.id === message.id && _message.version === message.version;
+        return _message.id === message.id && (0, import_semver3.satisfies)(message.version, _message.version);
       }
       if (isMessageLatestVersion && _message.id === message.id) {
         return true;
@@ -12508,7 +12509,7 @@ var filterCollection = (collection, options) => {
 var getEventCatalogConfigurationFile = (directory) => async () => {
   try {
     const path4 = (0, import_node_path12.join)(directory, "eventcatalog.config.js");
-    const configModule = await __nccwpck_require__(1488)(path4);
+    const configModule = await __nccwpck_require__(1757)(path4);
     return configModule.default;
   } catch (error) {
     console.error("Error getting event catalog configuration file", error);
@@ -70667,7 +70668,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", ({ value: true }));
 exports.generateGitHubCommentForSchemaReview = void 0;
 const mustache_1 = __importDefault(__nccwpck_require__(4827));
-const sdk_1 = __importDefault(__nccwpck_require__(5672));
+const sdk_1 = __importDefault(__nccwpck_require__(8945));
 const core_1 = __nccwpck_require__(9999);
 const COMMENT_MARKER = '<!-- eventcatalog-schema-review-comment -->';
 const TEMPLATE = `
@@ -70785,7 +70786,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", ({ value: true }));
 exports.task = void 0;
 const core = __importStar(__nccwpck_require__(9999));
-const sdk_1 = __importDefault(__nccwpck_require__(5672));
+const sdk_1 = __importDefault(__nccwpck_require__(8945));
 const path_1 = __importDefault(__nccwpck_require__(6928));
 const github_1 = __nccwpck_require__(5006);
 const eventcatalog_1 = __nccwpck_require__(3078);
