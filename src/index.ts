f52e12e1379d492e9570dea64b5e388604294062
strobsi <simon.s.strobel@daimlertruck.com>
2025-07-29 13:34:43 +0200

    Improve logging

--- src/index.ts
+++ src/index.ts
@@ -15,10 +15,12 @@ async function run(): Promise<void> {
     core.info(`Catalog directory: ${catalogDirectory}`);
     const task = core.getInput('task');
     core.info(`Task to execute: ${task}`);
+    /*
     if (!VALID_TASKS.includes(task)) {
       core.setFailed(`Invalid input for \`task\`. Must be one of: ${VALID_TASKS.join(', ')}.`);
       return;
-    }
+    } 
+    */
 
     core.info(`Starting task: ${task}`);
     const changedFilesForCatalog = await getChangedFilesInCatalogDirectory(octokit, context, catalogDirectory || undefined);
